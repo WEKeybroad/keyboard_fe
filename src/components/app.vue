ee1c01024b510eefcf2665bf882744fa8bf3d557
Cruyun <1045671796@qq.com>
2017-10-21 18:33:44 +0800

    add

--- src/components/app.vue
+++ src/components/app.vue
@@ -6,18 +6,25 @@
                     <router-link to="/">
                         <a v-on:click="to_mac" v-bind:class="{tab_light: mac}">Mac</a>
                     </router-link>
+                    <span> | </span>
                     <router-link to="/windows">
                         <a v-on:click="to_windows" v-bind:class="{tab_light: !mac}">Windows</a>
                     </router-link>
                 </div>
                 <div class="text_box">
-                    <input type="text" placeholder="快捷键" class="input_box">
-                    <div class="icon_box">
+                    <div class="drag_list inline_block full_height vertical_align">
+                        <select v-model="group" class="select full_height full_width">
+                            <option value="common">通用</option>
+                            <option value="indesign">Indesign</option>
+                            <option value="ps">PhotoShop</option>
+                        </select>
+                    </div>
+                    <input type="text" v-model.trim="input_word" placeholder="请输入你想搜索的快捷键" class="input_box inline_block full_height vertical_align">
+                    <div v-on:click="search" class="icon_box inline_block full_height vertical_align">
                         <svg viewBox="0 0 200 200" class="icon">
                                 <use xmlns:xlink="http://www.w3.org/2000/svg" xlink:href="#search_icon"></use>
                         </svg>
                     </div>
-                    <div class="search_icon"></div>
                 </div>
                 <router-view></router-view>
             </div>
@@ -34,7 +41,10 @@
     export default {
         data() {
             return {
-                mac: true
+                mac: true,
+                input_word: "",
+                group: "",
+                warn: false
             };
         },
         components: {
@@ -47,24 +57,65 @@
             },
             to_windows() {
                 this.mac = false
+            },
+            search() {
+                fetch('/key/', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            group: this.group, 
+                            
+                        })
+                    }).then(res => {
+                        if (res.ok)
+                        {}   
+                    })
             }
         }
     };
 </script>
 
 <style lang="scss">
+    .tab {
+        margin-top: 70px;
+        margin-bottom: 20px;
+        font-size: 20px;
+    }
+    
     .tab_light {
         color: #5389f9;
     }
     
     .input_box {
-        width: 200px;
-        height: 50px;
+        width: 75%;
+        border: 2px solid #5389f9;
+        font-size: 20px;
     }
     
     .icon_box {
-        height: 40px;
-        width: 100px;
+        width: 8%;
         background-color: #000;
+    }
+    
+    .text_box {
+        font-size: 0;
+        height: 45px;
+        margin-bottom: 100px;
+    }
+    
+    .drag_list {
+        width: 15%;
+        border: 2px solid #bfbfbf;
+    }
+    
+    .icon {
+        width: 100%;
+        height: 100%;
+    }
+    .select {
+        font-size: 20px;
     }
 </style>
