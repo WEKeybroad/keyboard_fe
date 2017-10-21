4add54fa073ef376d4cd6b3379e223008a28db9a
Cruyun <1045671796@qq.com>
2017-10-21 10:43:55 +0800

    init

---
+++ src/mac.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './components/mac.vue'
+// import style from './main.scss'
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+})
---
+++ src/windows.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import WinApp from './components/win.vue'
+// import style from './main.scss'
+
+new Vue({
+    el: "#winApp",
+    render: h => h(WinApp)
+})
